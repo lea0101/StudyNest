2845df2bc74e5bc09722f7b9df79378a794796d0
Ka1ea <gink@purdue.edu>
2024-09-29 21:12:19 -0400

    Fix Bugs for getting user data

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -4,15 +4,15 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 function Login() {
     const [user, loading, error] = useAuthState(auth)
     async function handleLogin() {
-        await signInWithPopup(auth, provider)
+        await signInWithPopup(auth, provider).catch(() => console.log("login error"))
     }
     return (
         <>
             <h1> Login Form </h1>
-            <button onClick={handleLogin}> </button>
+            <button onClick={handleLogin}> Login GOOGLE</button>
             {loading && <p>Loading!</p>}
-            {user && <><h2>{user[0]?.displayName}</h2>
-                <img src={user[0]?.photoUrl} alt="Error" /> </>}
+            {user && <><h2>{user?.displayName}</h2>
+                <img src={user?.photoURL} alt="profile_image" /> </>}
         </>
     )
 }
