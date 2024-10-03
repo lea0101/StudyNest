9058181778fe47c51104d6c8a333caffa0c78c78
Ka1ea <gink@purdue.edu>
2024-10-02 23:28:04 -0400

    Fix path error

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
-import Login from './Login';
-import Home from './Home';
+import Login from './Components/Login';
+import Home from './Components/Home';
 
 function App() {
   return (
