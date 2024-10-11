c4a432533dd16397b8657edcb7bb94b7d979ad60
advait-panicker <advait.panicker@yahoo.com>
2024-10-11 03:06:41 -0400

    Revert "Clear-all button added"
    This reverts commit 24245dbb1ab1d0d35420f11a22d86e6361842a4a.

--- whiteboard/src/components/P5Wrapper.js
+++ whiteboard/src/components/P5Wrapper.js
@@ -12,9 +12,9 @@ import {
 } from "firebase/firestore";
 import { db } from "../config/firebase";
 
-const P5Wrapper = ({ tool, color, fill, strokes, setStrokes }) => {
+const P5Wrapper = ({ tool, color, fill }) => {
     const sketchRef = useRef(null);
-    // let [strokes, setStrokes] = useState([]);
+    let [strokes, setStrokes] = useState([]);
 
     useEffect(() => {
         const q = query(
@@ -34,7 +34,7 @@ const P5Wrapper = ({ tool, color, fill, strokes, setStrokes }) => {
     }, []);
 
     async function addStroke(stroke) {
-        const docRef = await addDoc(collection(db, "strokes"), { ...stroke.toJSON(), createdAt: serverTimestamp() });
+        const docRef = await addDoc(collection(db, "strokes"), {...stroke.toJSON(), createdAt: serverTimestamp()});
         stroke.setID(docRef);
         strokes.push(stroke);
         setStrokes([...strokes]);
@@ -45,19 +45,6 @@ const P5Wrapper = ({ tool, color, fill, strokes, setStrokes }) => {
         // strokes = strokes.filter(stroke => stroke.id !== id);
         setStrokes([...strokes]);
     }
-
-    useEffect(() => {
-        if (strokes.length === 0) {
-            const q = query(
-                collection(db, "strokes")
-            );
-            onSnapshot(q, (QuerySnapshot) => {
-                QuerySnapshot.forEach((doc) => {
-                    deleteDoc(doc.ref);
-                });
-            });
-        }
-    }, [strokes]);
 
     useEffect(() => {
         const sketch = (p) => {
