c93778feb399c559f3a522c4b22feae322848d4e
Leanne Alsatie <lalsatie@purdue.edu>
2024-10-27 14:49:35 -0400

    Working on firebase sync but it's not actually working yet

--- client/src/components/FileFeature/FileViewer.js
+++ client/src/components/FileFeature/FileViewer.js
@@ -1,32 +1,195 @@
 import React, { useState, useEffect } from 'react';
-import { storage, auth } from '../../config/firebase';
+import { storage, auth, db } from '../../config/firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { ref, listAll, getDownloadURL, uploadBytesResumable, getStorage } from "firebase/storage";
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import FileUploader from "./FileUploader";
 import NavBar from "../Home/NavBar";
 import "./FileCollab.css";
-import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { Button, Position, PrimaryButton, Worker, Viewer, Tooltip } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import { Document, Page, pdfjs } from "react-pdf";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import { HighlightArea, SelectionData, highlightPlugin, RenderHighlightTargetProps, RenderHighlightContentProps, MessageIcon, RenderHighlightsProps } from '@react-pdf-viewer/highlight';
+import '@react-pdf-viewer/highlight/lib/styles/index.css';
+import { TiMessage } from "react-icons/ti";
 
+import {
+    query,
+    collection,
+    onSnapshot,
+    addDoc,
+    doc,
+    deleteDoc,
+    serverTimestamp
+} from "firebase/firestore";
+
+interface RenderHighlightProp {
+    fileUrl: string;
+}
+
+interface Note {
+    id: number;
+    content: string;
+    highlightAreas: HighlightArea[];
+    quote: string;
+    fileUrl: string;
+}
 
 const FileViewer = (props) => {
+    // Get the file url from firebase
     const [ numPages, setNumPages ] = useState(null);
     const [ pageNumber, setPageNumber ] = useState(1);
     const [ isLoading, setIsLoading ] = useState(true);
     const [ url, setURL ] = useState(null);
     const { state } = useLocation();
 
+    const [ message, setMessage ] = useState('');
+    const [ notes, setNotes ] = useState([]);
+    let noteId = notes.length;
+    const fileName = props.file;
+
     useEffect(() => {
-        getDownloadURL(ref(storage, props.file))
+        getDownloadURL(ref(storage, fileName))
             .then((url) => {
                 setURL(url);
                 setInterval(() => setIsLoading(false), 500);
             }).catch(() => setIsLoading(false));
-        return () => {};
+        const q = query(
+            collection(db, "pdf_notes")
+        );
+        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+            const fetchedNotes = [];
+            QuerySnapshot.forEach((doc) => {
+                let thisNote: Note = {
+                    id: ++noteId,
+                    content: doc.data().content,
+                    highlightAreas: doc.data().highlightAreas,
+                    quote: doc.data().quote,
+                    fileUrl: fileName
+                };
+                thisNote.setID(doc.ref);
+
+                if (thisNote?.fileName === fileName) {
+                    fetchedNotes.push(thisNote);
+                }
+            });
+            setNotes(fetchedNotes);
+        });
+        return () => unsubscribe;
     }, []);
+
+    // Handling highlights and notes
+    const renderHighlightTarget = (props: RenderHighlightTargetProps) => (
+        <div
+            style={{
+                background: '#eee',
+                display: 'flex',
+                position: 'absolute',
+                left: `${props.selectionRegion.left}%`,
+                top: `${props.selectionRegion.top + props.selectionRegion.height}%`,
+                transform: 'translate(0, 8px)',
+                zIndex: 1,
+            }}
+        >
+            <Tooltip
+                position={Position.TopCenter}
+                target={
+                    <Button onClick={props.toggle}>
+                        <MessageIcon />
+                    </Button>
+                }
+                content={() => <div style={{ width: '100px' }}>Add a note</div>}
+                offset={{ left: 0, top: -8 }}
+            />
+        </div>
+    );
+
+    async function addNote(note) {
+        const docRef = await addDoc(collection(db, "file_notes"));
+        note.setID(docRef);
+        console.log(note);
+    }
+    const renderHighlightContent = (props: RenderHighlightContentProps) => {
+        const addNote = () => {
+            if (message !== '') {
+                const note: Note = {
+                    id: ++noteId,
+                    content: message,
+                    highlightAreas: props.highlightAreas,
+                    quote: props.selectedText,
+                    fileUrl: fileName
+                };
+                setNotes(notes.concat([note]));
+                props.cancel();
+                // Send note to firestore.
+               addNote(note); 
+            }
+        };
+        return (
+            <div
+                style={{
+                    background: '#fff',
+                    border: '1px solid rgba(0, 0, 0, .3)',
+                    borderRadius: '2px',
+                    padding: '8px',
+                    position: 'absolute',
+                    left: `${props.selectionRegion.left}%`,
+                    top: `${props.selectionRegion.top + props.selectionRegion.height}%`,
+                    zIndex: 1,
+                }}
+            >
+                <div>
+                    <textarea
+                        rows={3}
+                        style={{
+                            border: '1px solid rgba(0, 0, 0, .3)',
+                        }}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        marginTop: '8px',
+                    }}
+                >
+                    <div style={{ marginRight: '8px' }}>
+                        <PrimaryButton onClick={addNote}>Add</PrimaryButton>
+                    </div>
+                    <Button onClick={props.cancel}>Cancel</Button>
+                </div>
+            </div>
+        );
+    };
+    const renderHighlights = (props: RenderHighlightsProps) => (
+        <div>
+            {notes.map((note) => (
+                <React.Fragment key={note.id}>
+                    {note.highlightAreas
+                        .filter((area) => area.pageIndex === props.pageIndex)
+                        .map((area, idx) => (
+                            <div
+                                key={idx}
+                                style={Object.assign(
+                                    {},
+                                    {
+                                        background: 'yellow',
+                                        opacity: 0.4,
+                                    },
+                                    props.getCssProperties(area, props.rotation)
+                                )}
+                            />
+                        ))}
+                </React.Fragment>
+            ))}
+        </div>
+    );
+
+    const highlightPluginInstance = highlightPlugin({renderHighlightTarget, renderHighlightContent, renderHighlights});
+
+
+
     if (isLoading) return <p>Loading...</p>;
     else {
         if (!url) {
@@ -36,7 +199,7 @@ const FileViewer = (props) => {
             return (
             <div className="file-viewer-container">
                 <div className="file-viewer">
-                    <Viewer fileUrl={url}/>
+                    <Viewer plugins={[highlightPluginInstance]} fileUrl={url}/>
                 </div>
              </div>
             );
