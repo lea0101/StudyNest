a44ab44763dec76e20ad5b2e7d02bab4ea3de53b
cadchan04 <chan377@purdue.edu>
2024-11-15 17:07:40 -0500

    basic UI of Meditation

--- client/src/components/BrainBreak/Meditation/Meditation.js
+++ client/src/components/BrainBreak/Meditation/Meditation.js
@@ -3,9 +3,43 @@ import { useParams, useNavigate, useLocation } from 'react-router-dom';
 
 import NavBar from '../../Home/NavBar';
 import NotAuthorizedPage from "../../../Pages/NotAuthorizedPage";
+import MeditationTimer from "./MeditationTimer";
 
 import '../BrainBreak.css'
+import './Meditation.css'
 import { useRoomSettings } from "../../Room/RoomSettingsContext";
+import { useTimer } from "../../Timer/TimerContext";
+
+import breathingExercisesImage from './images/breathing-exercise.png';
+import muscleRelaxationImage from './images/muscle-relaxation.jpg';
+import positiveAffirmationsImage from './images/positive-affirmations.png';
+
+const steps = [
+    {
+        id: 0,
+        title: "",
+        instructions: "Take a break from studying by following this step-by-step meditation guide! When you're ready to begin, click Begin."
+    },
+    {
+        id: 1,
+        title: "Breathing Exercises",
+        image: breathingExercisesImage,
+        instructions: "Sit comfortably, close your eyes, and take slow, deep breaths. Inhale for 4 seconds. Hold for 4 seconds. Exhale for 4 seconds. Repeat this for 2 minutes."
+    },
+    {
+        id: 2,
+        title: "Muscle Relaxation",
+        image: muscleRelaxationImage,
+        instructions: "Start from your toes and work your way up. Tense each muscle group for 5 seconds. Then release. Feel the stress and tension go away. Repeat this for 2 minutes."
+    },
+    {
+        id: 3,
+        title: "Positive Affirmations",
+        image: positiveAffirmationsImage,
+        instructions: "Repeat positive affirmations. I am calm and in control. I deserve peace. I am strong. I am smart. I am capable."
+    }
+
+];
 
 function Meditation() {
     const navigate = useNavigate();
@@ -13,31 +47,132 @@ function Meditation() {
     const { state } = useLocation(); // retrieve state (roomCode) passed when navigating
     const roomCode = state?.roomCode;
 
-    const { selectedColor, selectedLight } = useRoomSettings(); // access color and light settings
+    const { selectedColor, selectedLight, contextUserRole } = useRoomSettings(); // access color and light settings
+    const { isTimerDone, isActive, resetTimerStatus } = useTimer();
+
+    const [currStep, setCurrStep] = useState(0);
+
+    const backgroundColor = selectedLight === "light" ? "rgb(255, 253, 248)" : "rgb(69, 67, 63)";
+    const colorMapping = {
+        default: "#6fb2c5",
+        red: "rgb(217, 91, 91)",
+        orange: "rgb(204, 131, 53)",
+        yellow: "rgb(245, 227, 125)",
+        green: "rgb(118, 153, 93)",
+        blue: "rgb(59, 124, 150)",
+        purple: "rgb(165, 132, 224)",
+        pink: "rgb(242, 170, 213)"  
+    };
+    const buttonColor = colorMapping[selectedColor || colorMapping.default];
+
+    useEffect(() => {
+        if (isTimerDone && !isActive) {
+            alert("STUDY BREAK TIME !!!");
+            resetTimerStatus();
+        }
+    }, [isTimerDone, resetTimerStatus]);
+
+    const handleBegin = () => {
+        setCurrStep(1);
+    }
+
+    const handleBack = () => {
+        if (currStep > 0) {
+            setCurrStep(currStep - 1);
+        }
+    }
+
+    const handleNext = () => {
+        if (currStep < steps.length - 1) {
+            setCurrStep(currStep + 1);
+        }
+    }
 
     const handleGoBack = () => {
         navigate(`/rooms/${roomName}/brainbreak`, { state: {roomCode : roomCode}});
     }
 
     return (
-        <div>
-            <div className="hangman"
-            style={{
-                "background-color":
-                    selectedLight === "light"
-                    ? "white"
-                    : selectedLight === "dark"
-                    ? "rgb(69, 67, 63)"
-                    : "white",
-                "color":
-                    selectedLight === "light"
-                    ? "black"
-                    : selectedLight === "dark"
-                    ? "white"
-                    : "white"                       
-            }}>
+        <div
+        style={{
+            "backgroundColor": backgroundColor,   
+            "color": selectedLight === "light" ? "black" : "white"                    
+        }}>
+            <div className="meditation">
+
+                {/* Progress Bar */}
+                <div className="progress-bar">
+
+                </div>
+
+                {/* Start or Current Step */}
+                {currStep === 0 ? (
+                    <div>
+                        <h1>Meditation</h1>
+                        <p>{steps[0].instructions}</p>
+                        <button
+                            className="navigation-button"
+                            onClick={handleBegin} 
+                            style={{backgroundColor: buttonColor}}>
+                            Begin!
+                        </button>
+                    </div>
+                ) : (
+                    <div>
+                        <h1 style={{"color": selectedLight === "light" ? "black" : "white"}}>{steps[currStep].title}</h1>
+
+                        <div className="step-content">
+                            <img src={steps[currStep].image} alt={steps[currStep].title} className="step-image" />
+
+                            <div className="step-instructions">
+                                <p>{steps[currStep].instructions}</p>
+                                <div className="step-timer"> 
+                                    <MeditationTimer />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                )}
+
+                {/* Navigation Buttons */}
+                <div className="navigation">
+                    {currStep > 0 && (
+                        <button
+                        className="navigation-button"
+                        style={{backgroundColor: buttonColor}}
+                        onClick={handleBack}
+                        disabled={currStep === 0}
+                        >
+                            Back
+                        </button>
+                    )}
+                    
+                    {currStep > 0 && currStep < 3 && (
+                        <button
+                            className="navigation-button"
+                            style={{backgroundColor: buttonColor}}
+                            onClick={handleNext}
+                            disabled={currStep === 3}
+                        >
+                            Next
+                        </button>
+                    )}
+
+                    {currStep === 3 && (
+                        <button
+                        className="navigation-button"
+                        style={{backgroundColor: buttonColor}}
+                        onClick={handleGoBack}
+                    >
+                        Complete
+                    </button>
+                    )}
+                </div>
+
+
                 
-                <h1>Meditation</h1>
 
                 <div className="room-code" onClick={handleGoBack}>
                     <p>Go Back</p>
---
+++ client/src/components/BrainBreak/Meditation/MeditationTimer.js
@@ -0,0 +1,252 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { db } from '../../../config/firebase';
+import { doc, setDoc, getDoc, count, updateDoc, onSnapshot } from 'firebase/firestore';
+
+import { useRoomSettings } from '../../Room/RoomSettingsContext';
+import { useTimer } from '../../Timer/TimerContext';
+
+function MeditationTimer() {
+    const { roomCode, selectedLight, selectedColor } = useRoomSettings();
+    const { isTimerDone, notifyTimerDone, resetTimerStatus } = useTimer();
+
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [hoursInput, setHoursInput] = useState("");
+    const [minutesInput, setMinutesInput] = useState("");
+    const [secondsInput, setSecondsInput] = useState("");
+    const [countdownTime, setCountdownTime] = useState(null);
+    const [isActive, setIsActive] = useState(false);
+    const [paused, setPaused] = useState(false);
+    const [remainingTime, setRemainingTime] = useState(0);
+
+    // firestore reference for timer document
+    const timerRef = doc(db, 'rooms', roomCode);
+    const intervalRef = useRef(null); // store interval reference
+
+    // load timer state from firestore
+    useEffect(() => {
+        const unsubscribe = onSnapshot(timerRef, (doc) => {
+            if (doc.exists) {
+                try {
+                    const timerData = doc.data().timer;
+                    
+                    if (timerData) {
+                        const { countdownTime, remainingTime, isActive, paused } = timerData;
+
+                        if (isActive && countdownTime) {
+                            const adjustedCountdownTime = countdownTime - Date.now();
+                            setCountdownTime(countdownTime);
+                            setIsActive(true);
+                            setPaused(false);
+                            setRemainingTime(adjustedCountdownTime);
+                            updateDisplay(adjustedCountdownTime);
+                        } else if (paused) {
+                            setRemainingTime(remainingTime);
+                            setIsActive(false);
+                            setPaused(true);
+                        } else {
+                            setRemainingTime(remainingTime);
+                            setIsActive(false);
+                            setPaused(false);
+                        }
+                    }
+                } catch (error) {
+                    console.error("Error loading timer data: ", error);
+                }
+            }
+        });
+
+        return () => unsubscribe;
+
+        // const loadTimerData = async () => {
+        //     try {
+        //         const timerDoc = await getDoc(timerRef);
+        //         if (timerDoc.exists()) {
+        //             const timerData = timerDoc.data().timer;
+        //             if (timerData) {
+        //                 const { countdownTime, remainingTime, isActive, paused } = timerData;
+
+        //                 if (isActive && countdownTime) {
+        //                     const adjustedCountdownTime = countdownTime - Date.now();
+        //                     setCountdownTime(countdownTime);
+        //                     setIsActive(true);
+        //                     setPaused(paused);
+        //                     setRemainingTime(adjustedCountdownTime);
+        //                     updateDisplay(adjustedCountdownTime);
+        //                 } else {
+        //                     setRemainingTime(remainingTime);
+        //                 }
+        //             }
+        //         }
+        //     } catch (error) {
+        //         console.error("Error loading timer data: ", error);
+        //     }
+        // };
+        // loadTimerData();
+
+    }, [roomCode]);
+
+    // handle countdown timer interval
+    useEffect(() => {
+        if (isActive && countdownTime) {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+
+            const tick = () => {
+                const now = Date.now();
+                const timeLeft = countdownTime - now;
+
+                if (timeLeft > 0) {
+                    updateDisplay(timeLeft);
+                    setRemainingTime(timeLeft);
+                } else {
+                    clearInterval(intervalRef.current);
+                    intervalRef.current = null;
+                    resetTimer();
+                    handleComplete();
+
+                    console.log("STUDY BREAK TIME!!!");
+                }
+            };
+
+            tick();
+            intervalRef.current = setInterval(tick, 1000);
+
+            return () => clearInterval(intervalRef.current);
+        }
+    }, [isActive, countdownTime]);
+
+    const handleStart = async () => {
+        if (isActive && !paused) {
+            // pause timer
+            setPaused(true);
+            setIsActive(false); // stops interval without clearing remaining time
+
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+
+            await updateTimerInFirestore({ paused: true, isActive: false, remainingTime });
+        } else if (paused) {
+            // resume timer
+            const newCountdownTime = Date.now() + remainingTime;
+
+            setCountdownTime(newCountdownTime); // set new target time
+            setPaused(false);
+            setIsActive(true);
+
+            await updateTimerInFirestore({ countdownTime: newCountdownTime, paused: false, isActive: true });
+        } else {
+            // start timer for the first time
+            const totalMilliseconds = (hoursInput * 3600 + minutesInput * 60 + secondsInput) * 1000;
+            const endTimestamp = Date.now() + totalMilliseconds;
+
+            setCountdownTime(endTimestamp);
+            setRemainingTime(totalMilliseconds);
+            setPaused(false);
+            setIsActive(true);
+
+            // initialize display immediately
+            updateDisplay(totalMilliseconds);
+
+            await updateTimerInFirestore({ countdownTime: endTimestamp, remainingTime: totalMilliseconds, isActive: true, paused: false });
+        }
+        
+    }
+
+    const resetTimer = () => {
+        setIsActive(false);
+        setPaused(false);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
+        setHoursInput("");
+        setMinutesInput("");
+        setSecondsInput("");
+        setCountdownTime(null);
+        setRemainingTime(0);
+        if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+
+    const handleRestart = () => {
+        resetTimer();
+
+        updateDoc(timerRef, {
+            timer: {
+                countdownTime: null,
+                remainingTime: 0,
+                isActive: false,
+                paused: false,
+                initialDuration: 0
+            }
+        });
+    };
+
+    const handleComplete = async () => {
+        resetTimer();
+
+        alert("STUDY BREAK TIME!!");
+    
+        await updateDoc(timerRef, {
+            'timer.timerComplete': true, // Set flag in Firestore
+            'timer.isActive': false,
+            'timer.remainingTime': 0
+        });
+
+        // wait briefly before resetting completion status
+        setTimeout(() => {
+            resetTimerStatus();
+        }, 1000); // 1 second delay
+    };
+    
+
+    const updateDisplay = (timeLeft) => {
+        const newTimeLeft = timeLeft + 1000;
+        setHours(Math.floor(newTimeLeft / (1000 * 60 * 60)));
+        setMinutes(Math.floor((newTimeLeft % (1000 * 60 * 60)) / (1000 * 60)));
+        setSeconds(Math.floor((newTimeLeft % (1000 * 60)) / 1000));
+    }
+
+    const updateTimerInFirestore = async (timerData) => {
+        try {
+            await updateDoc(timerRef, { timer: timerData });
+        } catch (error) {
+            console.error("Error updating timer in Firestore: ", error);
+        }
+    }
+
+    return (
+        <div className="timer"
+        style={{
+            "background-color":
+                selectedLight === "light"
+                ? "white"
+                : selectedLight === "dark"
+                ? "rgb(100, 100, 100)"
+                : "white",                       
+        }}>
+            <div className="timer-container"
+            >
+                <h3>Meditation Timer</h3>
+
+                <div style={{display: 'flex', justifyContent: 'center', gap: '10px'}}>
+                    <input type="number" placeholder='Hours' value={hoursInput} onChange={(e) => setHoursInput(Number(e.target.value))}></input>
+                    <input type="number" placeholder='Minutes' value={minutesInput} onChange={(e) => setMinutesInput(Number(e.target.value))}></input>
+                    <input type="number" placeholder='Seconds' value={secondsInput} onChange={(e) => setSecondsInput(Number(e.target.value))}></input>
+                    <button className="dynamic-button" style={{width: "70px"}} onClick={handleStart}>
+                        {isActive && !paused ? "Pause" : paused ? "Resume" : "Start"}
+                    </button>
+                    <button className="dynamic-button" style={{width: "70px"}} onClick={handleRestart}>Restart</button>
+                </div>
+
+                <h1>{String(hours).padStart(2, '0')}:{String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default MeditationTimer;
