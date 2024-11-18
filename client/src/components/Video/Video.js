8dacb11825c36d1a70d21ac16700c59e9f051e11
advait-panicker <advait.panicker@yahoo.com>
2024-11-17 19:38:49 -0500

    Video queues now sync per room + video annotations synced per video in room

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -9,9 +9,13 @@ import {
     setDoc
 } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+import NotAuthorizedPage from "../../Pages/NotAuthorizedPage";
+
+import { useLocation } from 'react-router-dom';
 
 const Video = () => {
     const auth = getAuth();
@@ -21,6 +25,49 @@ const Video = () => {
     let [videoId, setVideoId] = useState('');
     let [displayName, setDisplayName] = useState('');
     let [photoURL, setPhotoURL] = useState('');
+
+    const [isAuthorized, setAuthorized] = useState(2);
+
+    const { state } = useLocation(); // retrieve state (roomCode) passed when navigating
+    const roomCode = state?.roomCode;
+
+    useEffect(() => {
+        console.log("useEffect 1");
+
+        if (loading) {
+            return;
+        }
+
+        if (roomCode == undefined) {
+            setAuthorized(1);
+            return;
+        }
+        
+        if (user) {
+            if (roomCode == undefined) {
+                setAuthorized(1);
+                return;
+            }
+            const roomDocRef = doc(db, 'rooms', roomCode);
+            getDoc(roomDocRef).then(doc => {
+                 if (doc.exists()) {
+                    const userList = doc.data().userList || {};
+                    const userInList = Object.values(userList).some(userObj => userObj.uid === user.uid);
+
+                    if (userInList) {
+                        setAuthorized(0); // set authorized if user is in userList
+                    } else {
+                        setAuthorized(1);
+                    }
+                 } else {
+                    setAuthorized(1);
+                 }
+            }).catch(error => {
+                console.error("Error fetching room: ", error);
+                setAuthorized(1);
+            })
+        }
+    }, [loading]);
 
     useEffect(() => {
         if (user) {
@@ -34,7 +81,7 @@ const Video = () => {
         // fetch annotations for the video
         const q = query(
             // TODO: incorporate videoId into query
-            collection(db, "youtube-annotations")
+            collection(db, `yt-com-${roomCode}-${videoId}`)
         );
         const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
             const newAnnotations = [];
@@ -45,13 +92,14 @@ const Video = () => {
             });
             const sortedAnnotations = newAnnotations.sort((a, b) => a.timestamp - b.timestamp);
             setAnnotations(sortedAnnotations);
+            console.log('updated annotations', sortedAnnotations);
         });
         return () => unsubscribe;
-    }, []);
+    }, [videoId]);
 
     async function updateDBTimestamp(newTimestamp) {
         const q = query(
-            collection(db, "youtube-timestamps")
+            collection(db, `yt-time-${roomCode}`)
         );
         const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
             QuerySnapshot.forEach((doc) => {
@@ -65,7 +113,7 @@ const Video = () => {
 
     useEffect(() => {
         const q = query(
-            collection(db, "youtube-timestamps")
+            collection(db, `yt-time-${roomCode}`)
         );
         const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
             let newTimestamp = timestamp;
@@ -97,9 +145,10 @@ const Video = () => {
         while (left <= right) {
             let mid = Math.floor((left + right) / 2);
             if (annotations[mid].timestamp === timestamp) {
-                annotations.splice(mid, 0, annotation);
-                setAnnotations([...annotations]);
-                return;
+                // annotations.splice(mid, 0, annotation);
+                // setAnnotations([...annotations]);
+                left = mid;
+                break;
             } else if (annotations[mid].timestamp < timestamp) {
                 left = mid + 1;
             } else {
@@ -107,8 +156,15 @@ const Video = () => {
             }
         }
         annotations.splice(left, 0, annotation);
-        const docRef = await addDoc(collection(db, "youtube-annotations"), annotation);
+        const docRef = await addDoc(collection(db, `yt-com-${roomCode}-${videoId}`), annotation);
         setAnnotations([...annotations]);
+        console.log('added new annotation', annotation, annotations);
+    }
+
+    if (isAuthorized == 1) {
+        return <NotAuthorizedPage/>
+    } else if (isAuthorized == 2){
+        return <div> Loading... </div>
     }
 
     return (
@@ -138,7 +194,7 @@ const Video = () => {
                     <button className='b-button' onClick={addAnnotation}>Submit</button>
                 </div>
             </div>
-            <VideoQueue setCurrentVideo={setVideoId}></VideoQueue>
+            <VideoQueue roomCode={roomCode} setCurrentVideo={setVideoId}></VideoQueue>
         </div>
     );
 }
