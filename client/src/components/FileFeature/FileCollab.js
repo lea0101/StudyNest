7f7103ce5ba62a81011ade8ee1b15eef22a97acb
Leanne Alsatie <lalsatie@purdue.edu>
2024-11-22 00:11:57 -0500

    Finishing bookmark feature

--- client/src/components/FileFeature/FileCollab.js
+++ client/src/components/FileFeature/FileCollab.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useReducer } from 'react';
 import { storage, auth } from '../../config/firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { ref, listAll, getDownloadURL, uploadBytesResumable } from "firebase/storage";
@@ -8,13 +8,23 @@ import FileViewer from "./FileViewer";
 import NavBar from "../Home/NavBar";
 import "./FileCollab.css";
 import "../../App.css";
+import { useRoomSettings } from "../Room/RoomSettingsContext";
+import { useTimer } from "../Timer/TimerContext";
 
 const FileCollab = () => {
+    const { selectedColor, selectedLight, contextUserRole } = useRoomSettings(); // access color & light and user role settings
+    const { isTimerDone, isActive, resetTimerStatus } = useTimer(); // access timer
+
+    const backgroundColor = selectedLight === "light" ? "rgb(255, 253, 248)" : "rgb(69, 67, 63)"; // add at the top
+    
+
+
     const [ files, setFiles ] = useState([]);
     const { state } = useLocation();
     const [ selectedFile, setSelectedFile ] = useState('');
     const roomName = state?.roomCode;
     const [ shouldUpdate, setShouldUpdate ] = useState(true);
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
     const loadFilesHandler = useCallback(() => {
         const getFileList = async () => {
@@ -28,15 +38,25 @@ const FileCollab = () => {
             const urls = await getFileList();
             setFiles(urls);
         }
-        getFiles();
+        getFiles().then(() => {
+            setShouldUpdate(!shouldUpdate);
+        });
     }, []);
 
     useEffect(() => {
         loadFilesHandler();
     }, []);
 
+    useEffect(() => {
+    }, [files, shouldUpdate]);
+
     return (
-        <div className="module">
+        <div className="module" 
+            style={{
+                "backgroundColor": backgroundColor, 
+                "color": selectedLight === "light" ? "black" : "white"
+            }}
+        >
             <NavBar />
             <br/>
             <br/>
